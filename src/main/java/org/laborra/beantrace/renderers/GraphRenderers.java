e0a9b75b2c4d9c9468b3c3bf5097f6c446514db4
Andrea Panattoni <panattoni.andrea@gmail.com>
2015-06-08 22:22:10 +0200

    Javadoc

--- src/main/java/org/laborra/beantrace/renderers/GraphRenderers.java
+++ src/main/java/org/laborra/beantrace/renderers/GraphRenderers.java
@@ -1,5 +1,12 @@
 package org.laborra.beantrace.renderers;
 
+/**
+ * Basic class to create graph renderers. They can be used to draw different king
+ * of bean traces.
+ *
+ * @see org.laborra.beantrace.BeanTraces#printBeanTrace(Object, GraphRenderer)
+ * @see org.laborra.beantrace.TraceConfiguration#setGraphRenderer(GraphRenderer)
+ */
 public class GraphRenderers {
     public static AsciiRenderer newAsciiRenderer(Appendable appendable) {
         return new AsciiRenderer(appendable);
