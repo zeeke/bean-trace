47ffdc104cbf45d55637c360c0b072b582318218
Andrea Panattoni <panattoni.andrea@gmail.com>
2015-06-20 09:10:38 +0200

    Docs examples

--- src/test/groovy/org/laborra/beantrace/ExampleDocs.groovy
+++ src/test/groovy/org/laborra/beantrace/ExampleDocs.groovy
@@ -8,10 +8,11 @@ class ExampleDocs {
     private File outputDirectory
 
     static void main(String ... args) {
-        def documentation = new ExampleDocs(outputDirectory: new File(args[0]))
+        def docs = new ExampleDocs(outputDirectory: new File(args[0]))
 
-        documentation.mockitoAscii()
-        documentation.mockitoGraphviz()
+        docs.mockitoAscii()
+        docs.mockitoGraphviz()
+        docs.beanTraceConfig();
     }
 
     private File createOutputFile(String fileName) {
@@ -28,6 +29,15 @@ class ExampleDocs {
         def subject = Mockito.mock(Runnable);
         File outputFile = createOutputFile('mockito_graphviz.dot')
         BeanTraces.printBeanTrace(subject, GraphRenderers.newGraphviz(outputFile));
+    }
+
+    private beanTraceConfig() {
+        def subject = TraceConfiguration.makeDefault()
+        File outputFile = createOutputFile('bean_trace_graphviz.dot')
+        BeanTraces.printBeanTrace(subject, GraphRenderers.newGraphviz(outputFile));
+
+        outputFile = createOutputFile('bean_trace_ascii.txt')
+        BeanTraces.printBeanTrace(subject, GraphRenderers.newAscii(outputFile));
     }
 }
 
